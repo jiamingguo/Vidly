0d3087179122194a8a0812890b7d46ed33068c27
jiamingguo <gjm1993217@gmail.com>
2017-04-05 17:20:01 -0700

    create movie mvc

--- Vidly/Controllers/MoviesController.cs
+++ Vidly/Controllers/MoviesController.cs
@@ -3,15 +3,20 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Vidly.Models;
 
 namespace Vidly.Controllers
 {
     public class MoviesController : Controller
     {
-        // GET: Movies
-        public ActionResult Index()
+        // GET: Movies/Random
+        public ActionResult Random()
+            // here the return type is ActionResult. It could be other types if necessary.
         {
-            return View();
+            var movie = new Movie(){ Name = "Shrek!"};
+
+            return View(movie);
+
         }
     }
 }
