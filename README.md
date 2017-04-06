84134eb3a10fbde6ba89f65037bff17ad71c79d7
jiamingguo <gjm1993217@gmail.com>
2017-04-06 00:31:10 -0700

    update RouteConfig

--- README.md
+++ README.md
@@ -18,3 +18,18 @@ a project used to learn C# and ASP.NET
 | FileResult            | File()             |
 | HttpNotFoundResult    | HttpNotFound()     |
 | EmptyResult           |                    |
+
+### Action Parameters
+- Parameter binding: Request -> MVC Framework -> Action
+- several ways to pass parameters to function
+  - in url: localhost:53982/movies/1
+  - also: localhost:53982/movies?pageIndex=1&sortBy=Name
+
+### Custom Route
+- goto RouteConfig.cs
+- before Default route, add a new route. Attention! Must before the default route
+### Attribute Routing
+- Easier and better way to do custom routing
+- The normal way to do Custom routing in previous session is also needed since the attribute routing enabled after 2013
+- `routes.MapMvcAttributeRoutes();` method instead of `routes.MapRoute()`
+- You can also add constraints to it with Regex. Just like the old ways
--- Vidly/Controllers/MoviesController.cs
+++ Vidly/Controllers/MoviesController.cs
@@ -15,8 +15,47 @@ namespace Vidly.Controllers
         {
             var movie = new Movie(){ Name = "Shrek!"};
 
-            return View(movie);
+            // return Content("HelloWorld");
+            // return HttpNotFound();
+            return RedirectToAction("Index", "Home", new {page = 1,sortBy = "name" }); // redirect to home page
 
+
+        }
+
+        // GET: movies/edit/#id 
+        // Also: movie/edit?id=1
+        public ActionResult Edit(int id)
+        {
+            return Content("id = " + id);
+        }
+        
+        // the '?' after "int" means Nullable. 
+        // string is a reference type, it could be null. int is a value type, it shouldn't be null.
+        // here we just want to make it nullalbe so the function can handle multiple cases
+        public ActionResult Index(int? pageIndex,string sortBy)
+        {
+            if (!pageIndex.HasValue)
+            {
+                pageIndex = 1;
+            }
+
+            if (String.IsNullOrWhiteSpace(sortBy))
+            {
+                sortBy = "Name";
+            }
+
+            return Content(String.Format("pageIndex = {0}&sortBy{1}", pageIndex, sortBy));
+        }
+
+        public ActionResult ByReleaseDate(int year, int month)
+        {
+            return Content(year + "/" + month);
+        }
+
+        [Route("movies/test/{year}/{month:regex(\\d{2}):range(1,12)}")]
+        public ActionResult AttributeRouteTest(int year, int month)
+        {
+            return Content("Test Attribute Route" + year + "/" + month);
         }
     }
 }
